0e0421b299f1454c8a9242e3878322f6719ca70c
iblearner <kienvi96@hotmail.com>
2021-05-06 11:33:23 +1000

    Added mysql as a dependancy and connected it to the db made in workbench.

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var mysql   = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Kienvi1996',
+    database : 'food-review'
+});
+ 
+connection.connect((err) => {
+    if (err) return console.log(`Error connecting: ${err.stack}`)
+    console.log(`Connected as id ${connection.threadId}`)
+});
+ 
+connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
 //app uses cors here
 
 app.get('/all', (req, res) => {
